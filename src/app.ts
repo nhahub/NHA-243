cfe7bda856c261718b7ba5565a9ce5437b754694
Mohamed Eid Latif <MohamedLatif@my.uopeople.edu>
2025-12-07 21:55:25 +0200

    feat: Add Prometheus monitoring and Kubernetes manifests
    - Add prom-client for metrics endpoint (/metrics)
    - Update docker-compose.yml with Prometheus, Grafana, cAdvisor
    - Add Kubernetes manifests (app, mongo, monitoring)
    - Update README with DevOps tools documentation

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoute from "./routes/userRoute";
 import productRoute from "./routes/productRoute";
 import cartRoute from "./routes/cartRoute";
+import client from "prom-client";
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -20,5 +21,18 @@ mongoose
 app.use("/user", userRoute);
 app.use("/products", productRoute);
 app.use("/cart", cartRoute);
+
+
+
+// Create a Registry to register the metrics
+const register = new client.Registry();
+
+// Add a default metrics collection
+client.collectDefaultMetrics({ register });
+
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.send(await register.metrics());
+});
 
 export default app;
